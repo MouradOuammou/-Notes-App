04523da3c080b84887259cbf003e942b02e452d1
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-23 16:36:30 +0100

    pro de logique

--- lib/screens/home/show_note.dart
+++ lib/screens/home/show_note.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
+import '../../models/noteModel.dart';
+
 class ShowNote extends StatefulWidget {
   final NoteModel noteData;
   final int index;
+  const ShowNote({Key? key, required this.noteData, required this.index}) : super(key: key);
+
 }
