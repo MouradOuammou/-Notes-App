2c88a72f9cfcd30904631d9d550c02572f79d566
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-23 16:36:04 +0100

    commencer show note

---
+++ lib/screens/home/show_note.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+class ShowNote extends StatefulWidget {
+  final NoteModel noteData;
+  final int index;
+}
