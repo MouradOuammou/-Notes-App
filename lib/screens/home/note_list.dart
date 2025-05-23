ee14c4ee70b534061858e3639503b3fc26cf0a5b
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-23 16:33:07 +0100

    ajouter some colors

--- lib/screens/home/note_list.dart
+++ lib/screens/home/note_list.dart
@@ -11,4 +11,18 @@ import '../../controllers/NoteController.dart';
 class NoteList extends StatelessWidget {
   final AuthController authController = Get.find<AuthController>();
   final NoteController noteController = Get.find<NoteController>();
+  final List<Color> lightColors = [
+    Colors.amber.shade300,
+    Colors.lightGreen.shade300,
+    Colors.lightBlue.shade300,
+    Colors.orange.shade300,
+    Colors.pinkAccent.shade100,
+    Colors.tealAccent.shade100,
+    Colors.purpleAccent,
+    Colors.greenAccent.shade400,
+    Colors.cyanAccent,
+  ];
+
+  final Random random = Random();
+
 }
