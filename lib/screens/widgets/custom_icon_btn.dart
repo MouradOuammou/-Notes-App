8544a2d44f5b8929ea76cec6710a8737e2c7bea2
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-23 16:14:18 +0100

    completer icon btn

--- lib/screens/widgets/custom_icon_btn.dart
+++ lib/screens/widgets/custom_icon_btn.dart
@@ -12,5 +12,18 @@ class CustomIconBtn extends StatelessWidget {
     required this.icon,
   }) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: icon,
+      ),
+    );
+  }
 }
