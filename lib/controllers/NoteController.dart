008af1c3ccefcf8cf444208b38fb40f2d727a823
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-23 15:59:43 +0100

    controller de note avec possibliter de ameliorer

---
+++ lib/controllers/NoteController.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../models/noteModel.dart';
+import 'AuthController.dart';
+
+class NoteController extends GetxController {
+  /// Liste observable des notes
+  final RxList<NoteModel> _noteList = <NoteModel>[].obs;
+
+  /// Contrôleurs de texte pour les champs titre et contenu
+  final titleController = TextEditingController();
+  final bodyController = TextEditingController();
+
+  /// Getter pour accéder à la liste de notes
+  List<NoteModel> get notes => _noteList;
+
+  @override
+  void onInit() {
+    super.onInit();
+    final uid = Get.find<AuthController>().user?.uid;
+    debugPrint("NoteController onInit :: $uid");
+
+    if (uid != null) {
+      _noteList.bindStream(Database().noteStream(uid));
+    } else {
+      debugPrint("Erreur : utilisateur non connecté");
+    }
+  }
+
+  @override
+  void onClose() {
+    titleController.dispose();
+    bodyController.dispose();
+    super.onClose();
+  }
+  void clearForm() {
+    titleController.clear();
+    bodyController.clear();
+  }
+}
