d4fbe6d96343d18b046a15b06d2e9b94735a32ac
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-23 15:55:18 +0100

    definir dossier home  widget settings auth and login.dart

---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_note/controllers/authController.dart';
+import 'package:flutter_note/controllers/userController.dart';
+import 'package:flutter_note/screens/auth/signup.dart';
+import 'package:get/get.dart';
+
+class Login extends StatelessWidget {
+  Login({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+  final AuthController authController = Get.find<AuthController>();
+  final UserController userController = Get.find<UserController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  'Hello there,\nWelcome back',
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Form(
+                  key: _formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: authController.email,
+                        validator: (value) {
+                          final email = value ?? '';
+                          return RegExp(
+                              r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$")
+                              .hasMatch(email)
+                              ? null
+                              : "Please enter a valid email";
+                        },
+                        decoration: InputDecoration(
+                          hintText: "EMAIL",
+                          prefixIcon: Icon(Icons.email, color: Colors.grey),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.background,
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        controller: authController.password,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Enter a password!';
+                          } else if (value.length < 6) {
+                            return "Password must be at least 6 characters";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          hintText: "PASSWORD",
+                          prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.background,
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.tealAccent.shade700,
+                    padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    minimumSize: const Size(250, 50),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      authController.login();
+                    }
+                  },
+                  child: const Text(
+                    "SIGN IN",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "New here?  ",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Get.to(() => SignUp());
+                      },
+                      child: const Text(
+                        "Sign up instead",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
