7d5e98d0cc9a3d20e9c994f93d35588e990cf175
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-23 15:58:37 +0100

    correction controller user

--- lib/controllers/UserController.dart
+++ lib/controllers/UserController.dart
@@ -3,12 +3,10 @@ import 'package:get/get.dart';
 import '../models/user.dart';
 
 class UserController extends GetxController {
-  Rx<UserModel> _userModel = UserModel(id: '', name: '', email: '').obs;
+  final Rx<UserModel> _userModel = UserModel(id: '', name: '', email: '').obs;
 
   UserModel get user => _userModel.value;
-
-  set user(UserModel value) => this._userModel.value = value;
-
+  set user(UserModel value) => _userModel.value = value;
   void clear() {
     _userModel.value = UserModel(id: '', name: '', email: '');
   }
